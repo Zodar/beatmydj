29c383037b73b6c7561f94402b4e674b43ce07cc
Tarik Merabet <tmerabet@sollan.com>
2016-11-22 16:07:30 +0100

    tchat fonctionne ! MF !

--- web/js/search.js
+++ web/js/search.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	$("#search-query").prop('disabled', true);
+	$("#search-query-tchat").prop('disabled', true);
+
 	var baseurl = $("input[name='baseurl']").val();
 	var photoPath = $("input[name='photo_path']").val();
 	getAllUsers();
@@ -32,19 +34,32 @@ $(document).ready(function(){
 
 function getAllUsers() {
 	var baseurl = $("input[name='baseurl']").val();
+	var photoPath = $("input[name='photo_path']").val();
+
 	$.ajax({data: $(this).serialize(), url: baseurl + "get_all_dj",
-		success: function(data){
+		success: function(data) {
 			var usernames = new Array();
+			var nbUsers = data.users.length - 1;
+			$(".button_chat.js-trigger").html("Chat (" + nbUsers + ")");
+			$(".chat__users").html("Utilisateurs: " + nbUsers);
+	    	var ownUserName = $("input[name='user_userName']").val();
+
 			$.each(data.users, function(k, v) {
-				usernames.push(v.userName);
+				if (ownUserName != v.userName) {
+					usernames.push(v.userName);
+					var chat_choice = " <li class='chat__human'> <img class='chat__avatar' src=\"" + photoPath + v.path + "\"  alt=\"" + v.userName + "\" /> ";
+					chat_choice += " <span class='chat__name'>" + v.userName + "</span> </li>";
+					$(".chat__wrapper").append(chat_choice);
+				}
 			});
 			$("#search-query").prop('disabled', false);
+			$("#search-query-tchat").prop('disabled', false);
 			$("#style-form-listeDJ").css('display', 'block');
 
 			$("#search-query").autocomplete({
 				source: usernames,
 				select: function(event, ui) {
-					if(ui.item){
+					if (ui.item) {
 						$('#search-query').val(ui.item.value);
 					}
 					window.location.href =  baseurl + "profil/" + ui.item.value;
