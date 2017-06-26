933ac1b1c423fd105e6ecf000a74dfaa16d575ef
Tarik Merabet <tmerabet@sollan.com>
2017-06-26 17:37:04 +0200

    suppression code sale inutile

--- src/BmdUserBundle/Controller/SecurityController.php
+++ src/BmdUserBundle/Controller/SecurityController.php
@@ -13,7 +13,7 @@ class SecurityController extends Controller
 {
     
     
-    /**
+  /**
      *
      *@Route("/login", name="login")
      * @param Request $request
@@ -49,57 +49,16 @@ class SecurityController extends Controller
     $pseudo = $find->findBy(array(
       "username" => $this->get('request')->query->get('_username')
     ));
-  // if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-  //       return new JsonResponse(array("user" => "deja connecte"));
-  // }
     if (count($pseudo) == 1) {
 
     $user = $pseudo[0];
     $token = new UsernamePasswordToken($user,$this->get('request')->query->get('_password'), "main", $user->getRoles());
     $this->get("security.context")->setToken($token);
-
-$this->get("security.context")->setToken($token);
-
     $event = new InteractiveLoginEvent($request, $token);
     $this->get("event_dispatcher")->dispatch("security.interactive_login", $event);
     return new JsonResponse(array("user" => $pseudo[0]->getEmail(),"token" => $token->serialize()));
   }
 
     return new JsonResponse(array("user" => "null"));
-
-
-    /*
-
-               $datas = [];
-     foreach ($pseudo as $user) {
-            $data = [];
-            $data["id"] = $user->getId();
-            $data["email"] = $user->getEmail();
-            $data["firstName"] = $user->getFirstName();
-            $data["lastName"] = $user->getLastName();
-            $data["userName"] = $user->getUserName();
-            $data["presentation"] = $user->getPresentation();
-            $data["style"] = $user->getStyle();
-            $data["dispo"] = $user->getDispo();
-            $data["path"] = $user->path;
-            array_push($datas, $data);
-        }
-
-
-$em = $this->getDoctrine();
-$repo  = $em->getRepository("UserBundle:User"); //Entity Repository
-$user = $repo->loadUserByUsername($username);
-if (!$user) {
-    throw new UsernameNotFoundException("User not found");
-} else {
-    $token = new UsernamePasswordToken($user, null, "your_firewall_name", $user->getRoles());
-    $this->get("security.context")->setToken($token); //now the user is logged in
-     
-    //now dispatch the login event
-    $request = $this->get("request");
-    $event = new InteractiveLoginEvent($request, $token);
-    $this->get("event_dispatcher")->dispatch("security.interactive_login", $event);
-}
-    */
   }
 }
